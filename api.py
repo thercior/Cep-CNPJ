41080c1ed6e824a3a02be27f0b68c27b6b1b1498
Thércio Rodrigues <thercior@gmail.com>
2024-02-26 15:29:12 -0300

    Criação da lista de estados e adequação das variáveis de endereço para tirar os espaços brancos na url de pesquisa

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import quote as url_quote
 
 
 def format_cep(cep_input):
@@ -8,14 +9,20 @@ def format_cep(cep_input):
     cep = cep[:8]
     return cep
 
+
 def cep_api(*args, **kwargs):
     # URL da ViaCEP
     print(args)
-    if 'cep' in args:
-        url = f"https://viacep.com.br/ws/{args['cep']}/json"
+    print(kwargs)
+    if args:
+        url = f"https://viacep.com.br/ws/{args[0]}/json"
         print(url)
-    elif 'uf' in args and 'localidade' in args and 'logradouro' in args:
-        url = f"https://viacep.com.br/ws/{args['uf']}/{args['localidade']}/{args['logradouro']}/json"
+    elif 'uf' in kwargs and 'localidade' in kwargs and 'logradouro' in kwargs:
+        uf = url_quote(kwargs['uf'])
+        localidade = url_quote(kwargs['localidade'])
+        logradouro = url_quote(kwargs['logradouro'])
+        url = f"https://viacep.com.br/ws/{uf}/{localidade}/{logradouro}/json"
+        print(url)
     else:
         raise ValueError("Parâmetros Inválidos para Busca")
 
@@ -26,7 +33,8 @@ def cep_api(*args, **kwargs):
         data = response.json()
         return data
     except requests.exceptions.RequestException as e:
-        raise Exception(f'Erro na requistição: {str(e)}')
+        raise Exception(f'Erro na requisição: {str(e)}')
+
 
 def cnpj_api(cnpj):
     ...
