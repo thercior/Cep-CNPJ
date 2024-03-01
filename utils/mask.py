7ade6e7384eaab58ef46837d88eacde15568c9f4
Thércio Rodrigues <thercior@gmail.com>
2024-02-29 22:05:21 -0300

    Tentando criar a máscara para input do cnpj

--- utils/mask.py
+++ utils/mask.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from streamlit.components.v1 import html
-# from components.inputs import input_cep
 
 
 def mask_cep():
@@ -23,7 +21,34 @@ def mask_cep():
     return st.markdown(html_str, unsafe_allow_html=True)
 
 
-def mask_cnpj(*args, **kwargs):
-    cnpj = args[0]
-    cnpj_mask = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}'
-    return cnpj_mask
+# def mask_cnpj(*args, **kwargs):
+#     cnpj = args[0]
+#     cnpj_mask = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}'
+#     return cnpj_mask
+
+
+def mask_cnpj():
+    script = """
+        <script>
+            // Função para aplicar a máscara de CNPJ
+            function aplicarMascaraCNPJ(valor) {
+                valor = valor.replace(/\D/g, ''); // Remove caracteres não numéricos
+                valor = valor.replace(/^(\d{2})(\d)/, '$1.$2'); // Adiciona ponto após os dois primeiros dígitos
+                valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3'); // Adiciona ponto após os três primeiros dígitos
+                valor = valor.replace(/\.(\d{3})(\d)/, '.$1/$2'); // Adiciona barra após os três dígitos seguintes
+                valor = valor.replace(/(\d{4})(\d)/, '$1-$2'); // Adiciona hífen após os quatro dígitos seguintes
+                return valor;
+            }
+
+            // Função para atualizar o valor do campo CNPJ
+            function atualizarCNPJ() {
+                var input = document.getElementById("cnpj_input");
+                input.value = aplicarMascaraCNPJ(input.value);
+            }
+
+            // Adiciona um ouvinte de eventos para atualizar o valor quando o usuário digitar
+            document.getElementById("cnpj_input").addEventListener("input", atualizarCNPJ);
+        <script>
+    """
+
+    return script
