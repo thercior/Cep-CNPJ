9c585ed5827b829d97c6d254f900eeb67d7a126a
Thércio Rodrigues <thercior@gmail.com>
2024-02-29 22:06:31 -0300

    Organizando a app principal de request cnpj

--- services/cnpj_main.py
+++ services/cnpj_main.py
@@ -1,54 +1,15 @@
-import locale
 import pandas as pd
 import streamlit as st
 from api.api import cnpj_api
 from components.button import button_search
 from utils.mask import mask_cnpj
+from utils.cnpj_utils import df_data, format_cnpj
 
-
-
-# Configurações da biblioteca para converter de um sistema para outro
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-
-
-def decimal_treat(decimal_str):
-    treat_value = locale.currency(float(decimal_str), grouping=True)
-    return treat_value
-
-
-def format_cnpj(cnpj_input):
-    cnpj = ''.join(filter(str.isdigit, cnpj_input))  #  Remove caracteres não numéricos
-    cnpj = cnpj[:14]  # Limita a 14 digitos
-    print(f"Este é o retorno do cnpj tratado: {cnpj}")
-    return cnpj
-
-
-def format_ativity(row):
-    
-    if row['atividade_principal']:
-        code = row['atividade_principal'][8][0]['code']
-        text = row['atividade_principal'][8][0]['text']
-        return f"Código CNAE: {code}\nAtividade: {text}"
-    elif row['atividades_secundarias']:
-        ativity_formats = []
-        for i, ativity in enumerate(row['atividades_secundarias'][9]):
-            code = ativity['code']
-            text = ativity['text']
-            ativity_formats.append(f"Atividade {i + 1}: Código CNAE: {code}, Atividade: {text}")
-        return "\n".join(ativity_formats) if ativity_formats else None
-    else:
-        return None
 
 def search_cnpj():
-    cnpj_input = st.text_input("Digite o CNPJ:", max_chars=18)
-    fields_company = [
-        "status", "ultima_atualizacao", "cnpj", "tipo", "porte", "nome",
-        "fantasia", "abertura", "atividade_principal", "atividades_secundarias",
-        "natureza_juridica", "logradouro", "numero", "complemento", "cep",
-        "bairro", "municipio", "uf", "email", "telefone", "situacao", "data_situacao",
-        "motivo_situacao", "capital_social",
-    ]
-    fields_qsa = ["nome", "qual"]
+    script = mask_cnpj()
+    st.markdown(script, unsafe_allow_html=True)
+    cnpj_input = st.text_input("Digite o CNPJ:", max_chars=18, key="cnpj_input", help="Digite somente números")
     bt_search = button_search("Consultar")
 
     if bt_search:
@@ -62,23 +23,15 @@ def search_cnpj():
 
         data_cnpj = cnpj_api(cnpj)
 
+        # Verifica se houve retorno de dados da request
         if data_cnpj:
             st.subheader(f"Resultado dos dados para o CNPJ {cnpj_input}")
-            # df_cnpj_qsa = pd.DataFrame(data_cnpj_qsa, index=fields_qsa)
 
             tab1, tab2 = st.tabs(["Dados da Empresa", "Quadro Social da Empresa"])
             with tab1:
                 st.write(f"Dados da empresa de CNPJ {cnpj_input}")
-                print(data_cnpj)
-                data_cnpj_company = [data_cnpj[field] for field in fields_company]
-                data_cnpj_company[-1] = decimal_treat(data_cnpj["capital_social"])
-                # key_fields = ['code', 'text']
-                # data_cnpj_company[8] = [{key: value for key, value in data.items() if key in [key_field for key_field in key_fields]} for data in data_cnpj]
-                # data_cnpj_company[9] = [{key: value for key, value in data.items() if key in [key_field for key_field in key_fields]} for data in data_cnpj]
-                df_cnpj_company = pd.DataFrame(data_cnpj_company, index=fields_company)
-                # df_cnpj_company['atividade_principal'] = df_cnpj_company.apply(format_ativity, axis=1)
-                # df_cnpj_company['atividades_secundaria'] = df_cnpj_company.apply(format_ativity, axis=1)
-                st.dataframe(df_cnpj_company, use_container_width=True)
+                df_cnpj = df_data(data_cnpj)  # tratar os dados da request
+                st.dataframe(df_cnpj, use_container_width=True)
 
             with tab2:
                 st.write(f"Quadro Social da empresa {data_cnpj['nome']}")
