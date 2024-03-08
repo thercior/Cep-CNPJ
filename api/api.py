d91f122a0847da242ccd477ec8a7b3b6312beec8
Thércio Rodrigues <thercior@gmail.com>
2024-03-08 12:04:27 -0300

    Criando e començando a estruturar o consumo da api da tabela fipe

--- api/api.py
+++ api/api.py
@@ -4,7 +4,6 @@ from urllib.parse import quote as url_quote
 
 
 def cep_api(*args, **kwargs):
-    # URL da ViaCEP
 
     # Argumentos para busca pelo cep
     if args:
@@ -42,6 +41,42 @@ def cnpj_api(cnpj):
         return data
     else:
         return None
+
+
+def fipe_api(*args, **kwargs):
+
+    if args:
+        fipe_id = args[0]
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/brands"
+
+    elif ('type_vehicle' and 'brands' and 'model' and 'year' and 'fipe_code') in kwargs:
+        type_vehicle = url_quote(kwargs['type_vehicle'])
+        brands = url_quote(kwargs['brands'])
+        model = url_quote(kwargs['model'])
+        year = url_quote(kwargs['year'])
+        fipe_code = url_quote(kwargs['fipe_code'])
+
+        case_brands = True if type_vehicle and brands else False
+        case_models = True if type_vehicle and brands and model else False
+        
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/brands/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/{fipe_code}/years/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/{fipe_code}/years/{year}/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/{fipe_code}/years/{year}/history/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/brands/{brands}/models/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/brands/{brands}/models/{model}/years/"
+        url = f"https://parallelum.com.br/fipe/api/v2/{type_vehicle}/brands/{brands}/models/{model}/years/{year}/"
+
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        if data:
+            return data
+        else:
+            return None
+    else:
+        raise ValueError("Parâmetros Inválidos para Busca")
 
 
 def ibge_api(uf):
