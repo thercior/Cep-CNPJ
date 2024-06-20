42cd0715181ffdc44995a6b4e0bc7cc4f58bdd3a
Thércio Rodrigues <thercior@gmail.com>
2024-06-20 20:04:47 -0300

    :fire: removendo estrutura inutilizada

--- components/inputs.py
+++
@@ -1,7 +0,0 @@
-# import streamlit as st
-# from utils.mask import mask_cep
-
-
-# def input_cep(label):
-#     mask_cep()
-#     return st.text_input(label, max_chars=10, key="cep_input", type="password")
